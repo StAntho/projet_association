bafb8efcda18acae07921d95503acb58417ec0d2
Anthony <anthony.stanix@gmail.com>
2021-12-22 19:13:40 +0100

    add registration admin

--- migrations/Version20211222173802.php
+++ migrations/Version20211222173802.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211222171317 extends AbstractMigration
+final class Version20211222173802 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\AdminType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -32,5 +41,38 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/newAdmin/{id}", name="new_admin")
+     */
+    public function addAdmin(Request $request, User $user): Response
+    {
+        $secret = "432156789cba";
+
+        $form = $this->createForm(AdminType::class);
+        $form->handleRequest($request);
+
+        // $user= $this->doctrine->getRepository(User::class)->find($id);
+        dump($user);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get("secret")->getData() === $secret) {
+                $user->setRoles(["ROLE_ADMIN"]);
+                $em = $this->doctrine->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                $this->addFlash("success", "Nouvel admin enregistré");
+
+                return $this->redirectToRoute("index");
+            } else {
+                throw $this->createAccessDeniedException("Vous n'avez pas les droits admin");
+            }
+        }
+
+        return $this->render("security/addAdmin.html.twig", [
+            'form' => $form->createView(),
+        ]);
     }
 }

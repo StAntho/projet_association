4047f4d0364e963cf4adf00917f5fb7b5f389a81
Evrard97 <Aka-evrard.aka@outlook.com>
2021-12-22 13:00:45 +0100

    add entity form controller  product

---
+++ src/Form/ProductType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
+        $builder
+            ->add('kind', TextType::class, [
+                'label' => 'Genre',
+            ])
+            ->add('type', TextType::class, [
+                'label' => 'Type',
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Prix',
+            ])
+            ->add('quantity', NumberType::class, [
+                'label' => 'Quantité',
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image (JPEG/PNG)',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Ajouter',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}

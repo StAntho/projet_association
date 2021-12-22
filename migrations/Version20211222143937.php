6309dc8d5f86fd0a883485c26129106c9802e51b
clannou <48106215+clannou@users.noreply.github.com>
2021-12-22 15:45:25 +0100

    add command entity, controller, template

--- migrations/Version20211222143937.php
+++ migrations/Version20211222143937.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211221105803 extends AbstractMigration
+final class Version20211222143937 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,13 @@ final class Version20211221105803 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, age INT NOT NULL, type VARCHAR(255) NOT NULL, race VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, gender VARCHAR(20) NOT NULL, sterilised TINYINT(1) NOT NULL, reserved TINYINT(1) NOT NULL, date_arrived DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE command (id INT AUTO_INCREMENT NOT NULL, status_command INT NOT NULL, date DATETIME NOT NULL, total_price NUMERIC(10, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE animal');
+        $this->addSql('DROP TABLE command');
     }
 }
